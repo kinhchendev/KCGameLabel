84e5c82153e97f3cd6ea76b7eca819698c5df0f5
Tran Vinh Kinh <tranvinhkinh@Trans-iMac.local>
2018-09-04 16:14:46 +0700

    init project

---
+++ KCGameLabel/KCGameLabel.h
@@ -0,0 +1,19 @@
+//
+//  KCGameLabel.h
+//  KCGameLabel
+//
+//  Created by Tran Vinh Kinh on 9/4/18.
+//  Copyright © 2018 Tran Vinh Kinh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KCGameLabel.
+FOUNDATION_EXPORT double KCGameLabelVersionNumber;
+
+//! Project version string for KCGameLabel.
+FOUNDATION_EXPORT const unsigned char KCGameLabelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KCGameLabel/PublicHeader.h>
+
+
